85ad3b267fde4fe470ddd9734f5ce34472a3c882
Surya Teja <113308141+Telusuga@users.noreply.github.com>
2023-04-30 11:42:10 +0530

    Update producer-v1.py

--- producer-v1.py
+++ producer-v1.py
@@ -11,13 +11,13 @@ def delivery_status(err,msg):
         print(f'The following messages are pushed to Topic--->{msg.topic()}')
 
 def login_info():
-    info={'bootstrap.servers':'pkc-6ojv2.us-west4.gcp.confluent.cloud:9092',
+    info={'bootstrap.servers':'xxxxxxxxxxxxxxx',#Bootstrap server is the place where my kafka cluster is running 
           'security.protocol':'SASL_SSL',
           'sasl.mechanism':'PLAIN',
-          'sasl.username':'GXPZJPTLQMOSBVKA',
-          'sasl.password':'c35F9t9ywdnocNNb1TAyGYaVmCIPynX0iPAWYxoe+ygDjBqBQA3hITu2cfykNtNt',
-          'acks':'1',
-          'partitioner':'consistent_random'
+          'sasl.username':'xxxxxxxxx',#API key can be used as username for sasl mechanism
+          'sasl.password':'xxxxxxxxxxx',#API secret can be used for password
+          'acks':'1',#Upon a successful delivery to the topic it will show a message
+          'partitioner':'consistent_random'#This parameter helps to send messages to any random partition in the topic
           }
     
     return info
@@ -39,13 +39,6 @@ while True:
 
 print('Flusing records')
 
-
-'''
-for i in range(0,10):
-    msg={'id':randint(1,1000),'Name':Faker('en_US').name(),'Country':Faker('en_US').country()}
-
-    p.produce(topic=t,value=str(msg),on_delivery=delivery_status)
-'''
 p.flush()
 
 
